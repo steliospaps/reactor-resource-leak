77ce275e15165c5197b07c0263a0dacc2ba470e8
stelios <stelios05@gmail.com>
2022-01-30 16:23:04 +0000

    issue reproduced

---
+++ src/main/java/io/github/steliospaps/reactive/cache/demo/DemoApplication.java
@@ -0,0 +1,65 @@
+package io.github.steliospaps.reactive.cache.demo;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class DemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+	public static void main(String[] args) throws InterruptedException {
+		//SpringApplication.run(DemoApplication.class, args);
+
+		EmitterProcessor<Object> completer = EmitterProcessor.create();
+		Mono<Object> completerMono = completer.ignoreElements().cache();
+
+
+		Flux<Long> a = Flux.interval(Duration.ofSeconds(1))
+			.takeUntilOther(completerMono)
+			.onBackpressureDrop(i->log.info("a dropping {}",i))
+			.log("a")
+			.cache(1)
+			;
+    Flux<Long> b = Flux.interval(Duration.ofSeconds(1)).log("b")
+			.onBackpressureDrop(i->log.info("b dropping {}",i))
+			.flatMap(i->Mono.just(i))
+			.cache(1);
+
+		Flux<String> c = Flux.combineLatest(a, b, (a1,b1)->a1+"."+b1)
+			.log("b+a");
+		
+		//Disposable cDisposable = c.subscribe();
+
+		Flux<String> d = Flux.interval(Duration.ofSeconds(1))
+			.filter(i->i==0)
+			.switchMap(i -> c)
+			.log("d");
+
+		Flux.interval(Duration.ofMinutes(1))//
+			.takeUntilOther(completerMono)
+			.filter(x -> false)//
+			.log("c")
+			.withLatestFrom(d, (i,j)->i+"."+j)
+			.log("c+b+a")
+			.flatMap(i->Mono.just(i))
+			.limitRate(6)
+			.takeUntilOther(completerMono)
+			.subscribe();
+		
+
+		TimeUnit.MINUTES.sleep(1);
+
+		completer.onComplete();
+		//cba.dispose();
+	}
+
+}
---
+++ src/test/java/io/github/steliospaps/reactive/cache/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.reactive.cache.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
